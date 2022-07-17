6e6ece96d5630055845949737c755c45037a85f5
sudhanshu-bh <bsudhanshu3@gmail.com>
2022-07-17 18:03:40 +0530

    smart contract completed

--- contracts/MintProfileImage.sol
+++ contracts/MintProfileImage.sol
@@ -5,20 +5,51 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-// contract Greeter {
-//     string private greeting;
+contract ProfileImageNfts is ERC721, Ownable {
+    using Counters for Counters.Counter;
+    using Strings for uint256;
+    
+    Counters.Counter _tokenIds;
 
-//     constructor(string memory _greeting) {
-//         console.log("Deploying a Greeter with greeting:", _greeting);
-//         greeting = _greeting;
-//     }
+    mapping(uint256 => string) _tokenURIs;
 
-//     function greet() public view returns (string memory) {
-//         return greeting;
-//     }
+    struct RenderToken {
+        uint256 id;
+        string uri;
+        string space;
+    }
 
-//     function setGreeting(string memory _greeting) public {
-//         console.log("Changing greeting from '%s' to '%s'", greeting, _greeting);
-//         greeting = _greeting;
-//     }
-// }
+    constructor() ERC721("ProfileImageNFTs", "PIN") {}
+
+    function _setTokenURI(uint256 tokenId, string memory _tokenURI) internal {
+        _tokenURIs[tokenId] = _tokenURI;
+    }
+
+    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+        require(_exists(tokenId), "URI does not exist on that ID");
+        string memory _RUri = _tokenURIs[tokenId];
+        return _RUri;
+    }
+
+    function getAllToken() public view returns (RenderToken[] memory) {
+        uint256 latestId = _tokenIds.current();
+        RenderToken[] memory res = new RenderToken[](latestId);
+        for(uint256 i=0; i <= latestId; i++) {
+            if(_exists(i)) {
+                string memory uri = tokenURI(i);
+                res[i] = RenderToken(i, uri, " ");
+            }
+        }
+
+        return res;
+    }
+    
+    function mint(address recipients, string memory _uri) public returns (uint256) {
+        uint256 newId = _tokenIds.current();
+        _mint(recipients, newId);
+        _setTokenURI(newId, _uri);
+        _tokenIds.increment();
+        return newId;
+    }
+
+}
